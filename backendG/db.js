a384d88bba16920088f56662347cbf6cba41dc1a
Gayatrimadhav <164062101+Gayatrimadhav@users.noreply.github.com>
2024-03-27 23:16:51 +0530

    Add files via upload

---
+++ backendG/db.js
@@ -0,0 +1,23 @@
+// const mongoose=require('mongoose');
+// const mongoURI="mongodb://localhost:27017"
+// const connectToMongo=()=>{
+//     mongoose.connect(mongoURI,()=>{
+//         console.log("connected");
+//     })
+// }
+// module.exports=connectToMongo;
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/load12"
+
+const connectToMongo = async () => {
+try {
+    mongoose.set('strictQuery', false)
+    mongoose.connect(mongoURI) 
+    console.log('Mongo connected1')
+}
+catch(error) {
+    console.log(error)
+    process.exit()
+}
+}
+module.exports = connectToMongo;
