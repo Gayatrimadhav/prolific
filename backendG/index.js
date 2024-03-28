45407fe51a3271035b6f705d93dcf2a35764358b
gayatrimadhav <gayatrikadhao@gmail.com>
2024-03-28 11:46:35 +0530

    changes

--- backendG/index.js
+++ backendG/index.js
@@ -3,12 +3,15 @@ const express = require('express');
 const cors = require('cors')
 
 connectToMongo();
-
+const path=require("path");
 const app = express()
 const port = 5008
 app.use(cors())
 app.use(express.json())
 
+const _dirname=path.dirname("")
+const buildpath = path.join(_dirname,"../frontend/build")
+app.use(express.static(buildpath));
 
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
